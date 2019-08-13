8d4ba7847bcae712e3bd25339d9d938e6c62222b
shaipe <shaipe@sina.com>
2019-08-13 10:08:25 +0800

    test three format get

--- git.sh
+++ git.sh
@@ -20,3 +20,15 @@ echo "git commit and push success"
 
 # 添加文件后需要在终端给脚本文件执行权限
 # chmod 777 git.sh
+
+# 1. 预览将要删除的文件
+# git rm -r -n --cached 文件/文件夹名称 
+# 加上 -n 这个参数，执行命令时，是不会删除任何文件，而是展示此命令要删除的文件列表预览。
+# 2. 确定无误后删除文件
+# git rm -r --cached 文件/文件夹名称
+# 3. 提交到本地并推送到远程服务器
+# git commit -m "提交说明"
+# git push origin master
+# 4. 修改本地 .gitignore 文件 并提交
+#   git commit -m "提交说明"
+#   git push origin master
