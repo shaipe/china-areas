934489aec74d5a583979566ef0db1912b2266d8f
shaipe <shaipe@sina.com>
2019-09-15 09:03:17 +0800

    write mca

--- src/mca.rs
+++ src/mca.rs
@@ -3,7 +3,37 @@
  * http://www.mca.gov.cn/article/sj/xzqh/2019
  */
 use crate::FileFormat;
+use scraper::{Html, Selector};
 
 pub fn start(f: FileFormat, sub_level: i32) {
     println!("{:?} {}, 正在抓紧开发中...", f, sub_level);
+
+    parse_html("http://www.mca.gov.cn/article/sj/xzqh/2019/201908/201908271607.html");
+}
+
+/// 获取和解析html
+pub fn parse_html(url: &str) {
+
+    let html = lane_net::get_str(url);
+    let doc = Html::parse_document(&html);
+    let selector = Selector::parse("tr").unwrap();
+    let td_selector = Selector::parse("td").unwrap();
+    let mut res: Vec<Vec<String>> = vec![];
+    for tr in doc.select(&selector) {
+        // println!("{:?}", tr.text());
+        // let x: Vec<String> = tr.text().map(|text| text.to_string()).collect();
+        // println!("{:?}", x);
+        let mut x = 0;
+        let mut xv: Vec<String> = vec![];
+        for td in tr.select(&td_selector) {
+            if x == 1 || x == 2 {
+                // xv.push(td.text().);
+            }
+            x += 1;
+        }
+        println!("{:?}", xv);
+        res.push(xv);
+    }
+
+    println!("{:?}", html);
 }
